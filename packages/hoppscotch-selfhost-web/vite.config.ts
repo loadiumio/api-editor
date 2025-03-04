a0a72c7b0845dcb4a11b64953cb6776e63014a33
MertcanElci <mertcan.elci@loadium.com>
2025-03-04 12:32:49 +0300

    Api editor fixes

--- packages/hoppscotch-selfhost-web/vite.config.ts
+++ packages/hoppscotch-selfhost-web/vite.config.ts
@@ -75,7 +75,7 @@ export default defineConfig({
       util: "util",
       querystring: "qs",
     },
-    dedupe: ["vue", "codemirror/state", "codemirror/view"],
+    dedupe: ["vue"],
   },
   plugins: [
     Inspect(),
