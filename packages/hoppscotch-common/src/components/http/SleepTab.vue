4ad9ec3785fcbc0078c8e277221d35b831367536
MertcanElci <mertcan.elci@loadium.com>
2025-03-17 12:27:41 +0300

    LOAD-6636-Add-Sleep-Button
    Signed-off-by: MertcanElci <mertcan.elci@loadium.com>

--- packages/hoppscotch-common/src/components/http/SleepTab.vue
+++ packages/hoppscotch-common/src/components/http/SleepTab.vue
@@ -20,6 +20,7 @@ import { useVModel } from "@vueuse/core"
 import { HoppTab } from "~/services/tab"
 import { HoppRequestDocument } from "~/helpers/rest/document"
 import { useI18n } from "@composables/i18n"
+import { editRESTRequest } from "~/newstore/collections";
 const props = defineProps<{ modelValue: HoppTab<HoppRequestDocument> }>()
 const emit = defineEmits<{
   (e: "update:modelValue", val: HoppTab<HoppRequestDocument>): void
@@ -35,6 +36,12 @@ watch(
     tab.value.document.request.params[0].value = updatedValue
       ? updatedValue.toString()
       : "0"
+    const saveCtx = tab.value.document.saveContext
+    if (saveCtx) {
+      const req = tab.value.document.request
+      editRESTRequest(saveCtx.folderPath, saveCtx.requestIndex, req)
+      tab.value.document.isDirty = false
+    }
   }
 )
 </script>
