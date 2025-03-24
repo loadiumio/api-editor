0c164467d0fd41a3d45affda7cae6a39e3634f75
MertcanElci <mertcan.elci@loadium.com>
2025-03-24 09:44:51 +0300

    LOAD-6677-Available-Variables
    Signed-off-by: MertcanElci <mertcan.elci@loadium.com>

--- packages/hoppscotch-common/src/components/http/AvailableVariables.vue
+++ packages/hoppscotch-common/src/components/http/AvailableVariables.vue
@@ -5,81 +5,39 @@
         class="sticky top-upperMobileSecondaryStickyFold z-10 flex flex-shrink-0 items-center justify-between overflow-x-auto border-b border-dividerLight bg-primary p-2 pl-4 sm:top-upperSecondaryStickyFold"
       >
         <label class="truncate font-semibold text-secondaryLight">
-          {{ t("request.json_path") }}
+          {{ t("request.run_time_variables") }}
         </label>
       </div>
       <div
-        v-for="(jsonVar, index) in availableVariables.jsonPathVariables"
-        :key="`json-${index}`"
+        v-for="(varName, index) in variableNames"
+        :key="`varName-${index}`"
         class="group flex divide-x divide-dividerLight border-b border-dividerLight"
       >
-        <SmartEnvInput v-model="jsonVar.varName" :is-disabled="true" />
-        <SmartEnvInput v-model="jsonVar.expression" :is-disabled="true" />
+        <div class="flex w-full bg-transparent px-4 py-2">
+          {{ varName }}
+        </div>
+        <HoppButtonSecondary :icon="IconCopy" @click="copy(varName)" />
       </div>
       <HoppSmartPlaceholder
-        v-if="availableVariables.jsonPathVariables.length < 1"
+        v-if="variableNames.length === 0"
         :src="`/api-editor/images/states/${colorMode.value}/add_files.svg`"
-        :alt="`${t('empty.run_time_json_variables')}`"
-        :text="t('empty.run_time_json_variables')"
-      />
-    </div>
-    <div>
-      <div
-        class="sticky top-upperMobileSecondaryStickyFold z-10 flex flex-shrink-0 items-center justify-between overflow-x-auto border-b border-dividerLight bg-primary p-2 pl-4 sm:top-upperSecondaryStickyFold"
-      >
-        <label class="truncate font-semibold text-secondaryLight">
-          {{ t("request.regex") }}
-        </label>
-      </div>
-      <div
-        v-for="(regexVar, index) in availableVariables.regexVariables"
-        :key="`regex-${index}`"
-        class="group flex divide-x divide-dividerLight border-b border-dividerLight"
-      >
-        <SmartEnvInput v-model="regexVar.varName" :is-disabled="true" />
-        <SmartEnvInput v-model="regexVar.expression" :is-disabled="true" />
-      </div>
-      <HoppSmartPlaceholder
-        v-if="availableVariables.regexVariables.length < 1"
-        :src="`/api-editor/images/states/${colorMode.value}/add_files.svg`"
-        :alt="`${t('empty.run_time_regex_variables')}`"
-        :text="t('empty.run_time_regex_variables')"
-      />
-    </div>
-    <div>
-      <div
-        class="sticky top-upperMobileSecondaryStickyFold z-10 flex flex-shrink-0 items-center justify-between overflow-x-auto border-b border-dividerLight bg-primary p-2 pl-4 sm:top-upperSecondaryStickyFold"
-      >
-        <label class="truncate font-semibold text-secondaryLight">
-          {{ t("request.css_selector") }}
-        </label>
-      </div>
-      <div
-        v-for="(cssVar, index) in availableVariables.cssSelectorVariables"
-        :key="`css-${index}`"
-        class="group flex divide-x divide-dividerLight border-b border-dividerLight"
-      >
-        <SmartEnvInput v-model="cssVar.varName" :is-disabled="true" />
-        <SmartEnvInput v-model="cssVar.expression" :is-disabled="true" />
-        <SmartEnvInput v-model="cssVar.attribute" :is-disabled="true" />
-        <SmartEnvInput v-model="cssVar.matchNumber" :is-disabled="true" />
-      </div>
-      <HoppSmartPlaceholder
-        v-if="availableVariables.cssSelectorVariables.length < 1"
-        :src="`/api-editor/images/states/${colorMode.value}/add_files.svg`"
-        :alt="`${t('empty.run_time_css_variables')}`"
-        :text="t('empty.run_time_css_variables')"
+        :alt="`${t('empty.run_time_variables')}`"
+        :text="t('empty.run_time_variables')"
       />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue"
+import { computed, ref} from "vue"
 import { useColorMode } from "@composables/theming"
 import { useI18n } from "@composables/i18n"
+import { copyToClipboard } from "@helpers/utils/clipboard"
+import IconCopy from "~icons/lucide/copy"
+import { useToast } from "@composables/toast"
 
 const t = useI18n()
+const toast = useToast()
 const colorMode = useColorMode()
 
 const props = defineProps<{
@@ -91,4 +49,23 @@ const props = defineProps<{
 }>()
 
 const availableVariables = ref(props.availableVariables)
+
+const variableNames = computed(() => {
+  const variableNames: string[] = []
+  availableVariables.value.jsonPathVariables.forEach((variable) =>
+    variableNames.push(`\${${variable.varName}}`)
+  )
+  availableVariables.value.cssSelectorVariables.forEach((variable) =>
+    variableNames.push(`\${${variable.varName}}`)
+  )
+  availableVariables.value.regexVariables.forEach((variable) =>
+    variableNames.push(`\${${variable.varName}}`)
+  )
+  return variableNames
+})
+
+const copy = (content: string) => {
+  copyToClipboard(content)
+  toast.success(`${t("state.copied_to_clipboard")}`)
+}
 </script>
